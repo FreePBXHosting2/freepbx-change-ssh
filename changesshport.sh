98eec31946c13343b3fa2016d8038f9460499f21
FreePBXHosting <aleach@cyberlynk.net>
2015-06-12 10:10:30 -0500

    add time to sshd backup filename
    & clean up comments

--- changesshport.sh
+++ changesshport.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+#############################################
 # FreePBXHosting.com - @freepbxhosting      #
-# VERSION 1.1  RELEASE DATE JUN 12 2015     #
+# VERSION 1.2  RELEASE DATE JUN 12 2015     #
 # DESC: CHANGES SSH PORT AND RESTARTS SSH   #
+#############################################
 
 # Prompt user for desired port
 echo -n "Please enter the port you would like SSH to run on > "
@@ -10,11 +11,10 @@ read SSHPORT
 echo ""
 
 # Create backup of current SSH config
-NOW=$(date +"%m_%d_%Y")
+NOW=$(date +"%m_%d_%Y-%H_%M_%S")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.inst.bckup.$NOW
 
 # Apply changes to sshd_config
-# sed -i -e "s/\#Port 22/Port $SSHPORT/" /etc/ssh/sshd_config
 sed -i -e "/Port /c\Port $SSHPORT" /etc/ssh/sshd_config
 echo -e "Restarting SSH in 5 seconds. Please wait.\n"
 sleep 5
