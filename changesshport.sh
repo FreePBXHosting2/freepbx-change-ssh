8452198b6210a67970cf6fe6b9be64e95a079f8f
FreePBXHosting <aleach@cyberlynk.net>
2015-06-12 09:46:27 -0500

    Update changesshport.sh

--- changesshport.sh
+++ changesshport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # FreePBXHosting.com - @freepbxhosting      #
-# VERSION 1.0  RELEASE DATE JUN 12 2015     #
+# VERSION 1.1  RELEASE DATE JUN 12 2015     #
 # DESC: CHANGES SSH PORT AND RESTARTS SSH   #
 
 # Prompt user for desired port
@@ -14,7 +14,7 @@ NOW=$(date +"%m_%d_%Y")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.inst.bckup.$NOW
 
 # Apply changes to sshd_config
-sed -i -e 's/\#Port 22/Port $SSHPORT/' /etc/ssh/sshd_config
+sed -i -e "s/\#Port 22/Port $SSHPORT/" /etc/ssh/sshd_config
 
 echo -e "Restarting SSH in 5 seconds. Please wait.\n"
 sleep 5
